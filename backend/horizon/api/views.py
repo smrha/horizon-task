7be2d02e26d5f5874f17dd6022825ffa37d7484d
smrha <lord.smrha@gmail.com>
2021-02-14 00:49:46 +0330

    Create horizon app

---
+++ backend/horizon/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+from rest_framework.response import Response
+
+@api_view()
+@permission_classes((permissions.AllowAny,))
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
