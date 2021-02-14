86922f4083257614ae66ab0c90ffc3ee11b4ac20
smrha <lord.smrha@gmail.com>
2021-02-14 21:58:07 +0330

    Install frontend framework

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,13 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import 'antd/dist/antd.css';
-import CustomLayout from './containers/Layout';
 
 function App() {
   return (
     <div className="App">
-      <CustomLayout>
-        this content
-      </CustomLayout>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
